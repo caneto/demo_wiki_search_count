a6720ea48fc4e2b247a81cfcea40b2e6196ac267
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2021-06-07 12:51:31 -0300

    Teste Wiki Kotlin Retrofit2

---
+++ app/src/main/java/br/com/capsistema/wikisearchcount/Model.kt
@@ -0,0 +1,7 @@
+package br.com.capaistema.wikisearchcount
+
+object Model {
+    data class Result(val query: Query)
+    data class Query(val searchinfo: SearchInfo)
+    data class SearchInfo(val totalhits: Int)
+}
